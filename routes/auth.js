6aa53f03341facdcfc3bd39b8f7e49b59d32cbe4
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2022-04-28 22:44:28 -0600

    Added bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,21 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 
 // Register
 router.post("/register", async (req, res) => {
     
     try{
+        // Generate secure password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
         // Create new user
         const newUser = new User({
             username:req.body.username,
             email:req.body.email,
-            password:req.body.password
+            password:hashedPassword
         });
 
         //  Save user and return response
