26fde747f7b65fb4b91de9982f4a32c87141307d
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2022-04-29 23:43:08 -0600

    added login response

--- routes/auth.js
+++ routes/auth.js
@@ -15,7 +15,7 @@ router.post("/register", async (req, res) => {
         const newUser = new User({
             username:req.body.username,
             email:req.body.email,
-            password:hashedPassword
+            password:hashedPassword,
         });
 
         //  Save user and return response
@@ -26,6 +26,31 @@ router.post("/register", async (req, res) => {
         console.log(err);
     }
 });
+
+
+//Login
+router.post("/login", async (req, res) => {
+
+    try {
+
+        //Find one user inside the document using the User module by passing in email
+        const user = await User.findOne({ email: req.body.email });
+        //If there is no user status not found
+        !user && res.status(404).json("user not found");
+
+        // Compares secure generated bcrypt password with user's actual password
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+        // If password is not valid, send bad request response
+        !validPassword && res.status(400).json("Incorrect Password");
+
+        // If user inputs correctly, sends status 200 and user data
+        res.status(200).json(user);
+
+    } catch(err) {
+        console.log(err);
+    }
+})
 
 
 module.exports = router;
