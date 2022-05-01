db76595c8bcba652a5d0b03775b125e3e00542fb
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2022-04-30 20:44:34 -0600

    added to user model, started user route

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcrypt");
 // Register
 router.post("/register", async (req, res) => {
     
-    try{
+    try {
         // Generate secure password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
+const User = require("../models/User");
 const router = require("express").Router();
 
 // User routes here
 router.get("/", (req, res) => {
     res.send("this is the users routes")
 });
+
+// Update User
+
+
+// Delete User
+
+
+// Get a User
+
+
+// Follow a User
+
+
+// Unfollow a user
+
 
 
 module.exports = router;
