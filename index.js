4285ea7be27ff666a60e31e03c6fec6f58a2fea1
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2022-05-01 23:22:30 -0600

    added post schema and started post route

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require("morgan");
 
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/post");
 
 const app = express();
 
@@ -29,6 +30,7 @@ app.use(morgan("common"));
 //Address for rest API
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 
 app.listen(8080, () => {
