8b522715ff5b1e5cd7cf157543a6c255a164c32b
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2022-05-02 21:04:35 -0600

    added like and dislike to posts route

--- routes/posts.js
+++ routes/posts.js
@@ -31,13 +31,50 @@ router.put("/:id", async (req, res) => {
             res.status(403).json("You can only update your own posts");
         }
     } catch(err) {
+        // Respond with internal server error
         res.status(500).json(err);
     }
 });
 
 // Delete post
+router.delete("/:id", async (req, res) => {
+    try {
+        // find id within the requests paramater
+        const post = await Post.findById(req.params.id);
+        // check if owner of post is the same. If it is the same, update, if not return error
+        if (post.userId === req.body.userId) {
+            // Updates post by setting the requests body
+            await post.deleteOne({$set:req.body});
+            res.status(200).json("Post has been deleted");
+        } else {
+            // Returns response with forbidden error
+            res.status(403).json("You can only delete your own posts");
+        }
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
 
-// Like post
+// Like or dislike post
+router.put("/:id/like", async (req, res) => {
+    try {
+        // find post by parameter id
+        const post = await Post.findById(req.params.id);
+        //Checks if post's like array includes this user or not, if it does it likes, if not dislike
+        if (!post.likes.includes(req.body.userId)) {
+            // Push userId to like array and respond with success code
+            await post.updateOne({ $push:{ likes: req.body.userId } });
+            res.status(200).json("post has been liked");
+        } else {
+            // pull likes array 
+            await post.updateOne({ $pull: { likes: req.body.userId } });
+            res.status(200).json("post has been disliked");
+        }
+    } catch(err) {
+        // Respond with internal server error
+        res.status(500).json(err)
+    }
+});
 
 // Get post
 
